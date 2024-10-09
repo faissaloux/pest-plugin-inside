253028dfffbc051612b1ef74ad9924d45423edd5
faissaloux <fwahabali@gmail.com>
2024-10-09 16:59:54 +0100

    scan directory

--- src/Expectation.php
+++ src/Expectation.php
@@ -7,27 +7,44 @@ use Pest\Expectation as PestExpectation;
 expect()->extend(
     'toReturnLowercase',
     function (): PestExpectation {
-        $file = $this->value;
+        $files = [$this->value];
 
-        if (! file_exists($file)) {
-            expect(true)->toBeFalse("$file not found!");
-        }
+        if (is_dir($files[0])) {
+            $directory = $files[0];
 
-        $content = include $file;
-
-        expect($content)->toBeArray();
-
-        // Clean up content from numerics and special characters.
-        $cleanContent = array_map(function ($word): string {
-            return preg_replace('/[^A-Za-z]/', '', $word);
-        }, $content);
-
-        foreach ($cleanContent as $key => $word) {
-            if ($word === '') {
-                continue;
+            if ($files = scandir($files[0])) {
+                $files = array_diff($files, ['.', '..']);
+                $files = array_map(fn (string $file): string => $directory.DIRECTORY_SEPARATOR.$file, $files);
             }
 
-            expect($word)->toBeLowercase("Not lowercase word found: $content[$key]");
+            if ($files === []) {
+                expect(true)->toBeTrue();
+
+                return $this;
+            }
+        }
+
+        foreach ((array) $files as $file) {
+            if (! file_exists($file)) {
+                expect(true)->toBeFalse("$file not found!");
+            }
+
+            $content = include $file;
+
+            expect($content)->toBeArray();
+
+            // Clean up content from numerics and special characters.
+            $cleanContent = array_map(function (string $word): string {
+                return (string) preg_replace('/[^A-Za-z]/', '', $word);
+            }, $content);
+
+            foreach ($cleanContent as $key => $word) {
+                if ($word === '') {
+                    continue;
+                }
+
+                expect($word)->toBeLowercase("Not lowercase word found: $content[$key]");
+            }
         }
 
         return $this;
--- tests/toReturnLowercase.php
+++ tests/toReturnLowercase.php
@@ -12,6 +12,16 @@ it('passes with not', function (): void {
         ->not->toReturnLowercase();
 });
 
+it('passes when directory is e mpty', function (): void {
+    expect('tests/Fixtures/empty')
+        ->toReturnLowercase();
+});
+
+it('passes when all directory files content are lowercase', function (): void {
+    expect('tests/Fixtures/shouldAllBeLowercase')
+        ->toReturnLowercase();
+});
+
 it('fails', function (): void {
     expect('tests/Fixtures/returnsArrayLowercaseWithUppercase.php')
         ->toReturnLowercase();
@@ -24,5 +34,15 @@ it('fails with not', function (): void {
 
 it('fails when file does not exist', function (): void {
     expect('tests/Fixtures/notExist.php')
+        ->toReturnLowercase();
+})->throws(ExpectationFailedException::class);
+
+it('fails when directory does not exist', function (): void {
+    expect('tests/Fixtures/notExist')
+        ->toReturnLowercase();
+})->throws(ExpectationFailedException::class);
+
+it('fails when not all directory files content are lowercase', function (): void {
+    expect('tests/Fixtures/shouldAllBeLowercase1')
         ->toReturnLowercase();
 })->throws(ExpectationFailedException::class);
