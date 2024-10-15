7e2abc2a4b2ab82b7ccd4a5d6f34a62bc969d9ee
faissaloux <fwahabali@gmail.com>
2024-10-15 13:11:59 +0100

    group expectation in Expectation class

--- src/Expectation.php
+++ src/Expectation.php
@@ -2,53 +2,28 @@
 
 declare(strict_types=1);
 
+namespace Faissaloux\PestInside;
+
 use Pest\Expectation as PestExpectation;
 
-expect()->extend(
-    'toReturnUnique',
-    function (int $depth = -1): PestExpectation {
-        $files = [$this->value];
+final class Expectation
+{
+    public function __construct(private string $value) {}
 
+    /**
+     * @return PestExpectation<string>
+     */
+    public function toReturnLowercase(int $depth = -1): PestExpectation
+    {
+        $files = [$this->value];
+    
         if (is_dir($files[0])) {
             $files = getFilesIn($files[0], $depth);
 
             if ($files === []) {
                 expect(true)->toBeTrue();
 
-                return $this;
-            }
-        }
-
-        foreach ((array) $files as $file) {
-            if (! file_exists($file)) {
-                expect(true)->toBeFalse("$file not found!");
-            }
-
-            $content = include $file;
-
-            expect($content)->toBeArray();
-
-            $duplicates = array_diff_assoc($content, array_unique($content));
-
-            expect($duplicates)->toBeEmpty('Duplicates found:'.implode(',', $duplicates)." in $file");
-        }
-
-        return $this;
-    }
-);
-
-expect()->extend(
-    'toReturnLowercase',
-    function (int $depth = -1): PestExpectation {
-        $files = [$this->value];
-
-        if (is_dir($files[0])) {
-            $files = getFilesIn($files[0], $depth);
-
-            if ($files === []) {
-                expect(true)->toBeTrue();
-
-                return $this;
+                return new PestExpectation($this->value);
             }
         }
 
@@ -75,6 +50,40 @@ expect()->extend(
             }
         }
 
-        return $this;
+        return new PestExpectation($this->value);
     }
-);
+
+    /**
+     * @return PestExpectation<string>
+     */
+    public function toReturnUnique(int $depth = -1): PestExpectation {
+        $files = [$this->value];
+    
+        if (is_dir($files[0])) {
+            $files = getFilesIn($files[0], $depth);
+
+            if ($files === []) {
+                expect(true)->toBeTrue();
+
+                return new PestExpectation($this->value);
+            }
+        }
+
+        foreach ((array) $files as $file) {
+            if (! file_exists($file)) {
+                expect(true)->toBeFalse("$file not found!");
+            }
+
+            $content = include $file;
+
+            expect($content)->toBeArray();
+
+            $duplicates = array_diff_assoc($content, array_unique($content));
+
+            expect($duplicates)->toBeEmpty('Duplicates found:'.implode(',', $duplicates)." in $file");
+        }
+
+        return new PestExpectation($this->value);
+    }
+        
+}
