e2de4b4fe54db85517c4767c219a91cde5df0107
faissaloux <fwahabali@gmail.com>
2024-10-21 20:32:47 +0100

    Inside class

--- src/Expectation.php
+++ src/Expectation.php
@@ -4,64 +4,11 @@ declare(strict_types=1);
 
 namespace Faissaloux\PestInside;
 
-use Pest\Expectation as PestExpectation;
-
-final class Expectation
+final class Expectation extends Inside
 {
-    /**
-     * @var array<string>
-     */
-    private array $files;
-
-    public function __construct(private string $value)
+    public function toReturnLowercase(int $depth = -1): void
     {
-        $this->files = [$value];
-    }
-
-    private function fetchFilesIfDirectory(int $depth): void
-    {
-        if (is_dir($this->files[0])) {
-            $this->files = getFilesIn($this->files[0], $depth);
-        }
-    }
-
-    private function checkFileExistence(string $file): void
-    {
-        if (! file_exists($file)) {
-            expect(true)->toBeFalse("$file not found!");
-        }
-    }
-
-    /**
-     * @return array<string>
-     */
-    private function getContentFrom(string $file): array
-    {
-        $content = include $file;
-
-        expect($content)->toBeArray();
-
-        return $content;
-    }
-
-    /**
-     * @return PestExpectation<string>
-     */
-    public function toReturnLowercase(int $depth = -1): PestExpectation
-    {
-        $this->fetchFilesIfDirectory($depth);
-
-        if ($this->files === []) {
-            expect(true)->toBeTrue();
-
-            return new PestExpectation($this->value);
-        }
-
-        foreach ($this->files as $file) {
-            $this->checkFileExistence($file);
-
-            $content = $this->getContentFrom($file);
-
+        $this->applyOnDirectory($depth, function ($file, $content) {
             // Clean up content from numerics and special characters.
             $cleanContent = array_map(function (string $word): string {
                 return (string) preg_replace('/[^A-Za-z]/', '', $word);
@@ -74,62 +21,26 @@ final class Expectation
 
                 expect($word)->toBeLowercase("Not lowercase word detected: $content[$key] in $file");
             }
-        }
-
-        return new PestExpectation($this->value);
+        });
     }
 
-    /**
-     * @return PestExpectation<string>
-     */
-    public function toReturnUnique(int $depth = -1): PestExpectation
+    public function toReturnUnique(int $depth = -1): void
     {
-        $this->fetchFilesIfDirectory($depth);
-
-        if ($this->files === []) {
-            expect(true)->toBeTrue();
-
-            return new PestExpectation($this->value);
-        }
-
-        foreach ($this->files as $file) {
-            $this->checkFileExistence($file);
-
-            $content = $this->getContentFrom($file);
-
+        $this->applyOnDirectory($depth, function (string $file, array $content): void {
             $duplicates = array_diff_assoc($content, array_unique($content));
 
             expect($duplicates)->toBeEmpty('Duplicates found: '.implode(',', $duplicates)." in $file");
-        }
-
-        return new PestExpectation($this->value);
+        });
     }
 
-    /**
-     * @return PestExpectation<string>
-     */
-    public function toReturnSingleWords(int $depth = -1): PestExpectation
+    public function toReturnSingleWords(int $depth = -1): void
     {
-        $this->fetchFilesIfDirectory($depth);
-
-        if ($this->files === []) {
-            expect(true)->toBeTrue();
-
-            return new PestExpectation($this->value);
-        }
-
-        foreach ($this->files as $file) {
-            $this->checkFileExistence($file);
-
-            $content = $this->getContentFrom($file);
-
+        $this->applyOnDirectory($depth, function (string $file, array $content): void {
             $notSingle = array_filter($content, function (string $word): bool {
                 return str_contains(trim($word), ' ');
             });
 
             expect($notSingle)->toBeEmpty('Not single words detected: '.implode(',', $notSingle)." in $file");
-        }
-
-        return new PestExpectation($this->value);
+        });
     }
 }
